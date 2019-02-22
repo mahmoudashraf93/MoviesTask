5a594024138b09f2467fd010a44d8cc32aa9aea6
mahmoud ashraf <mahmoud28@gmail.com>
2019-02-23 01:33:58 +0200

    movies screen

---
+++ MoviesTask/ViewControllers/Movies/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  MoviesTask
+//
+//  Created by mahmoud ashraf on 2/22/19.
+//  Copyright © 2019 SideProject. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
